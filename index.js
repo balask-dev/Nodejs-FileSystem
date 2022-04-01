b5f7be3b92f0c54188a0516aa1e3e383f4ed54c8
balask-dev <balask1225@gmail.com>
2022-04-01 12:12:43 +0530

    updated

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const {writeFile, readdir} = require('fs');
 const app = express();
 app.use(express.json());
+dotenv.config();
+
+const PORT = process.env.PORT;
+
 app.get("/create", function (req, res) { //create txt file
     var date = new Date();
     var timestamp = date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds();
@@ -26,4 +30,4 @@ app.get("/create", function (req, res) { //create txt file
 app.get("/",(req,res)=>{  
     res.send("connected")
 })
-app.listen(3000);
+app.listen(PORT);
