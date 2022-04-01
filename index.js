d36a89c6415df23d0d7b043077a4ac4626ae1b6d
Bala <92292670+balask-dev@users.noreply.github.com>
2022-04-01 12:37:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,30 +4,35 @@ const app = express();
 app.use(express.json());
 
 //creating a text file that contains timestamp details
+
 app.get("/create", function (req, res) { 
     var date = new Date();
     var timestamp = date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds();
     var fileName = date.toString();
     var txt = timestamp;
+
     //read directory
     readdir("./file", (err, data) => {  
       if (data.length > 5) {
           res.send("limit exceeded");
-      } else {
-          // writeFile in txt 
-      writeFile(`./file/${fileName}.txt`, txt, (err,data) =>    
-      console.log("done")
+      }
+        else 
+      {
+          // writeFile in txt format
+      writeFile(`./file/${fileName}.txt`, txt, (err,data) => console.log("done")
         );
         res.send(`created ${fileName}`); 
     }
     });
-  });
-//read directory 
+    });
+
+//read directory that contains txt file
   app.get("/timestamp", function (req, res) {
     readdir("./file", (err, data) => {
         res.send(data);
     })
 })
+
 app.get("/",(req,res)=>{  
     res.send("connected")
 })
